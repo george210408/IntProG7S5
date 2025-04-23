d311e1b948491944b8ecee47491349afb9e6e3f2
george210408 <jadelgadotquamv.edu.ni>
2025-04-23 14:01:42 -0600

    ejercicio 5

---
+++ ejercicio4.py
@@ -0,0 +1,5 @@
+ed = int(input("ingrese su edad: "))
+if (ed> 18):
+    print ("puede votar")
+else:
+    print ("no puede votar")
---
+++ ejercico5.py
@@ -0,0 +1,5 @@
+numero = int(input("ingrese el numero: ")) 
+if(numero > 0 and numero%2==0):
+    print ("su numero es positivo y par")
+else:
+    print("no cumple los requisitos")
